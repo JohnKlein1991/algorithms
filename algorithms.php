c36306e0717959e76d88a3d4f6ebdd521c0b10f6
andrew_marhai <buk2018irinam@gmail.com>
2021-07-18 15:45:07 +0300

    Added Dijkstra's algorithm for searching path with min cost in graph

--- algorithms.php
+++ algorithms.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @param array $numbers
- * @return array
+ * @param int[] $numbers
+ * @return int[]
  */
 function bubble_sort(array $numbers): array
 {
@@ -30,8 +30,8 @@ function bubble_sort(array $numbers): array
 }
 
 /**
- * @param array $numbers
- * @return []|array
+ * @param int[] $numbers
+ * @return int[]
  */
 function qsort(array $numbers): array
 {
@@ -58,4 +58,85 @@ function qsort(array $numbers): array
 
 
     return array_merge(qsort($less), [$numbers[$index]], qsort($greater));
+}
+
+/**
+ * @param array $graph array like
+ * [nodeName => cost, ...]
+ *   nodeName - string
+ *   cost - int
+ * @param int|string $start the name of the initial node
+ * @param int|string $finish the name of the destination node
+ * @return array|null array like
+ * ['path' => path, 'cost' => cost]
+ *   path - array, path from start to finish (including edges)
+ *   cost - int, cost of this path
+ */
+function dijkstra(array $graph, $start, $finish): ?array
+{
+    $findLowerCost = function (array $costs, array $processed) {
+        $result = null;
+        foreach ($costs as $nodeName => $cost) {
+            if (in_array($nodeName, $processed) || is_null($cost)) {
+                continue;
+            }
+
+            if (is_null($result)) {
+                $result = $nodeName;
+                continue;
+            }
+
+            if ($cost < $costs[$result]) {
+                $result = $nodeName;
+            }
+        }
+        return $result;
+    };
+
+    $costs = [];
+    $parents = [];
+    $processed = [];
+
+    $costs[$finish] = null;
+    $parents[$start] = null;
+    foreach ($graph[$start] as $nodeName => $cost) {
+        $costs[$nodeName] = $cost;
+        $parents[$nodeName] = $start;
+    }
+
+    $node = $findLowerCost($costs, $processed);
+
+    while (!is_null($node)) {
+        foreach ($graph[$node] as $nodeName => $cost) {
+            $newCost = $cost + $costs[$node];
+            if (!isset($costs[$nodeName])) {
+                $costs[$nodeName] = $newCost;
+                $parents[$nodeName] = $node;
+            } elseif ($newCost < $costs[$nodeName]) {
+                $costs[$nodeName] = $newCost;
+                $parents[$nodeName] = $node;
+            }
+        }
+        $processed[] = $node;
+        $node = $findLowerCost($costs, $processed);
+    }
+
+    if (!isset($costs[$finish])) {
+        return null;
+    }
+
+    $result = [
+        'path' => [],
+        'cost' => $costs[$finish],
+    ];
+
+    $currentElement = $finish;
+    $result['path'][] = $currentElement;
+    while (!is_null($currentElement) && isset($parents[$currentElement])) {
+        $result['path'][] = $parents[$currentElement];
+        $currentElement = $parents[$currentElement];
+    }
+    $result['path'] = array_reverse($result['path']);
+
+    return $result;
 }
